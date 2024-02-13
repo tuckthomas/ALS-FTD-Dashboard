ea09503829d7eae6162c84bf4930ab7d223747c2
Tucker Thomas Olson <90152609+tuckthomas@users.noreply.github.com>
2024-02-13 00:08:15 -0500

    Update urls.py
    Updated to urls.py to reflect recent updates to the views.py functions.

--- ALS_FTD_Research_Dashboard/urls.py
+++ ALS_FTD_Research_Dashboard/urls.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from Dashboard.views import fetch_trials, fetch_trials_csv, scrape_alsod_gene_list
+from Dashboard.views import process_and_save_trials_data
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fetch_trials/', fetch_trials, name='fetch_trials'),
-    path('fetch_trials_csv/', fetch_trials_csv, name='fetch_trials_csv'),
-    path('scrape_alsod_gene_list/', scrape_alsod_gene_list, name='scrape_alsod_gene_list'),
+    path('process_and_save_trials_data/', process_and_save_trials_data, name='process_and_save_trials_data'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    scrape_alsod_gene_list
