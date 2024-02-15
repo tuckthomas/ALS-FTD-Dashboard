861b27c1710334c8f913f128636d2db58fcb3448
Tucker Thomas Olson <90152609+tuckthomas@users.noreply.github.com>
2024-02-14 20:31:16 -0500

    Update api.py

--- Dashboard/api.py
+++ Dashboard/api.py
@@ -11,8 +11,10 @@ from django.conf import settings
 
 router = Router()
 
-# More of a placeholder for now, until the front-end is developed.
-# Or, until I issue an SSL certificate on my reverse proxy server. If I do so before front-end development.
+# The API function is more of a placeholder for now, until the front-end is developed.
+# Or, until I issue an SSL certificate on my reverse proxy server.
+# Howver, the imported 'get_trials' utils.py function is 100% functional.
+
 @router.get("/", tags=["Trials"])
 def get_trials(request):
     update_data()  # Update the database records first
