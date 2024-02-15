530a3616fe382a35ea933d172d866f388bb1da45
Tucker Thomas Olson <90152609+tuckthomas@users.noreply.github.com>
2024-02-14 20:30:04 -0500

    Update api.py

--- Dashboard/api.py
+++ Dashboard/api.py
@@ -12,8 +12,7 @@ from django.conf import settings
 router = Router()
 
 # More of a placeholder for now, until the front-end is developed.
-# Or, until I issue an SSL certificate on my reverse proxy server.
-# Whichever comes first.....
+# Or, until I issue an SSL certificate on my reverse proxy server. If I do so before front-end development.
 @router.get("/", tags=["Trials"])
 def get_trials(request):
     update_data()  # Update the database records first
